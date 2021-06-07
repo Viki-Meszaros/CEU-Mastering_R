4f3a9853813510456394756c8f0017dbbe3c23be
Viki-Meszaros <mviki97@gmail.com>
2021-06-07 21:51:29 +0200

    change assert_numeric to assert_number in forint

--- R/forint.R
+++ R/forint.R
@@ -2,13 +2,13 @@
 #' @param x number
 #' @return string
 #' @export
-#' @importFrom checkmate assert_numeric
+#' @importFrom checkmate assert_number
 #' @importFrom scales dollar
 #' @examples
 #' forint(42)
 #' forint(10000.42365)
 #' forint(1:10)
 forint <- function(x){
-  assert_numeric(x)
+  assert_number(x)
   dollar(x, prefix = "", suffix = " HUF")
 }
